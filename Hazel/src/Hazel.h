32c0be20c7b19d6215cd41b443f2065035e88b63
bukajiushang <240662665@qq.com>
2023-06-17 17:54:48 +0800

    Setup basic Application and Entry Point.

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -2,8 +2,8 @@
 
 // for use by hazel applications
 
-#include <stdio.h>
 #include "Hazel/Application.h"
+#include "Hazel/Log.h"
 
 // -----Entry Point-----------------
 #include "Hazel/EntryPoint.h"
--- Hazel/src/Hazel/Core.h
+++ Hazel/src/Hazel/Core.h
@@ -9,3 +9,5 @@
 #else
     #error Hazel only support Windows!
 #endif // HZ_PLATFORM_WINDOWS
+
+#define BIT(x) (1 << x)
